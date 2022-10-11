43b9279e89714e45fd51c302c017e572b42b7de4
Grian <grianguy32@gmail.com>
2022-10-11 07:44:55 +0300

    e

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
 
 serve(async (req: Request) => {
-    let text = await (await fetch("file:///main.html")).text()
+    let text = await (await fetch("file:///src/main.html")).text()
 
     new Response(text)
 });
