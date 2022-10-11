e177ae31c63c0e10f60453f617a4a3b2da8b94d6
Grian <grianguy32@gmail.com>
2022-10-11 07:42:03 +0300

    stuff

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
 import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
 
-serve((req: Request) => new Response("Hello World"));
+serve((req: Request) => 
+    new Response(Deno.readTextFileSync("./main.html"))
+);
