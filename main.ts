175c42a8f8daff37f08a892ad2f6cb6eb70af59f
Grian <grianguy32@gmail.com>
2022-10-11 07:46:19 +0300

    content type

--- main.ts
+++ main.ts
@@ -3,5 +3,9 @@ import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
 serve(async (req: Request) => {
     let text = await (await fetch("file:///src/main.html")).text()
 
-    new Response(text)
+    new Response(text, {
+        headers: {
+            "content-type": "text/html"
+        }
+    })
 });
