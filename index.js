00b03ca90db5fdc44ab0b38e482d284077161ebe
Grian <grianguy32@gmail.com>
2022-10-11 08:28:27 +0300

    eee

--- index.js
+++ index.js
@@ -6,12 +6,22 @@ let foragingXpPerBox = {
     "gold": 1610,
 }
 
-let levels = { 0: 0 }
+let levels = { "0": 0 }
 
 let res = await fetch("/levels.txt")
 
 let str = await res.text()
 
+let arrs = str.split("\n").filter((i) => i != "{{!}}-" && i != "{{!}}}" && i != "}}").map((i) => i.split("{{!}}{{!}}"))
+
+for (let i of arrs) {
+    let lvl = i[0].trim().split(" ")[1]
+    let xp = parseInt(i[2].trim().replaceAll(",", ""))
+
+    levels[lvl] = xp
+}
+
 console.log(levels)
-console.log(str.split("\n").map(() => split("	")))
+
+
 })()
